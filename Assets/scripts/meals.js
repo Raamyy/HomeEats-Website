7af49115333ffa67ac1c5b25420e688dbbf29eda
Salma-tharwat <salmatharwat51@gmail.com>
2020-04-27 02:53:56 +0200

    Collapse Added

--- Assets/scripts/meals.js
+++ Assets/scripts/meals.js
@@ -7,3 +7,18 @@ $(document).ready(function () {
         $("#loginModal").modal();
     });
 });
+$(document).ready(function () {
+    // executes when HTML-Document is loaded and DOM is ready
+    console.log("document is ready");
+
+
+    $(".card").hover(
+        function () {
+            $(this).addClass('shadow-lg').css('cursor', 'pointer');
+        }, function () {
+            $(this).removeClass('shadow-lg');
+        }
+    );
+
+    // document ready  
+});
